82a7dc2d3b458dfccb003548f1e52b8eaf48e9ff
Tomas Barrueto <tobarrueto@gmail.com>
2025-04-29 15:13:57 -0400

    backend is configured

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
